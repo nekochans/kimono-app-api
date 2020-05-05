ef6f2413c56546a037676f23d6add3189e4b97b3
kobayashi-m42 <m42kobayashi@gmail.com>
2020-05-05 21:47:17 +0900

    :sparkles: #3 アクセスログの出力を追加

--- main.go
+++ main.go
@@ -3,27 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/nekochans/kimono-app-api/infrastructure"
+	"github.com/nekochans/kimono-app-api/infrastructure/httputil"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	logOpts := infrastructure.LoggerOptions{}
-	logger, err := infrastructure.NewLoggerFromOptions(logOpts)
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[ERROR] Failed to create logger: %s\n", err)
-		os.Exit(1)
-	}
-
-	defer logger.Sync()
-	logger.Info("info test log")
-
 	fmt.Fprint(w, "Hello, HTTPサーバ")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8888", nil)
+	router := http.NewServeMux()
+	router.HandleFunc("/", handler)
+	http.ListenAndServe(":8888", httputil.Log(router))
 }
