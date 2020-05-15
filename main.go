402e4619c96d92fcde67625ea119a4d85cdd8ece
kobayashi-m42 <m42kobayashi@gmail.com>
2020-05-16 00:05:54 +0900

    :pencil2: #8 不要なコメントを削除

--- main.go
+++ main.go
@@ -2,13 +2,6 @@ package main
 
 import "github.com/nekochans/kimono-app-api/infrastructure"
 
-//func handler(w http.ResponseWriter, r *http.Request) {
-//	fmt.Fprint(w, "Hello, HTTPサーバ")
-//}
-
 func main() {
 	infrastructure.StartHTTPServer()
-	//router := http.NewServeMux()
-	//router.HandleFunc("/", handler)
-	//http.ListenAndServe(":8888", httputil.Log(router))
 }
